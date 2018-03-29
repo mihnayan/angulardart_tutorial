12b85bec3d64a602c2adcf61c3f3fd8a7e4e055f
Mikhail Mangushev <dev.mihnayan@gmail.com>
2018-03-29 06:52:33 +0300

    Add HeroService

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,21 +1,32 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
+import 'dart:async';
 
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, HeroComponent]
+  directives: [coreDirectives, HeroComponent],
+  providers: [const ClassProvider(HeroService)]
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Обзор героев';
-  List<Hero> heroes = mockHeroes;
+  final HeroService _heroService;
+  List<Hero> heroes;
   Hero selectedHero;
-  // Hero get myHero => heroes.first;
+  
+  AppComponent(this._heroService);
+  
+  @override
+  void ngOnInit() => _getHeroes();
 
   void onSelect(Hero hero) => selectedHero = hero;
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+  }
+
 }
