2e6a9b64d810eea949658cd33a02f5232ac0eef8
Mikhail Mangushev <dev.mihnayan@gmail.com>
2018-04-02 09:59:23 +0300

    Add top 4 heroes.

--- lib/src/dashboard/dashboard_component.dart
+++ lib/src/dashboard/dashboard_component.dart
@@ -1,9 +1,25 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
+
+import '../model/hero.dart';
+import '../services/hero_service.dart';
 
 @Component(
   selector: 'hero-dashboard',
-  template: '<h3>Панель управления</h3>'
+  templateUrl: 'dashboard_component.html',
+  directives: [coreDirectives]
 )
-class DashboardComponent {
+class DashboardComponent implements OnInit {
+  List<Hero> heroes;
+
+  final HeroService _heroService;
+
+  DashboardComponent(this._heroService);
+  
+  @override
+  Future<void> ngOnInit() async {
+    heroes = (await _heroService.getAll()).skip(1).take(4).toList();
+  }
 
 }
